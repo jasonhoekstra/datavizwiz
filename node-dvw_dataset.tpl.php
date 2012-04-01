8ca63ecf6e801925a117ddb5bb0295d45a1e2663
Jason Hoekstra <jasonhoekstra@gmail.com>
2012-03-31 21:55:29 -0400

    Adding mapping with Leaflet

--- node-dvw_dataset.tpl.php
+++ node-dvw_dataset.tpl.php
@@ -59,7 +59,7 @@
     <div class="terms terms-inline"><?php print $terms ?></div>
   <?php endif;?>
   </div>
-
+    <!--<div id="dvwmap01" style="width:100%; height:400px;"/></div>-->
   <div class="content">
     <?php print $content ?>
   </div>
@@ -74,6 +74,9 @@
   <?php if ($submitted): ?>
     <p><span class="submitted">Last updated: <?php print $submitted ?></span></p>
   <?php endif; ?>
+
+
+
 
 
 </div>

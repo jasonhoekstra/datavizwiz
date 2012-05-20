460a843def90f15306e3fe4d46a4290473db2b23
Jason Hoekstra <jasonhoekstra@gmail.com>
2012-05-20 16:51:23 -0400

    Moving and formatting functions to datavizwiz.js.  Simpler language in summary pane admin.

--- scripts/datavizwiz.js
+++ scripts/datavizwiz.js
@@ -63,10 +63,49 @@ function flotBar(paneID, variable, dataset, tickset) {
       label: variable, 
       data: dataset, 
       color: "#08519C",
-      bars: { show: true, barWidth: 0.3, align: "center" } 
+      bars: { show: true, barWidth: 0.3, align: "center" },
+      grid: { hoverable: true }
     } ],
     { xaxis: { ticks: tickset } }
   );
+  
+  var previousPoint = null;
+  $("#" + paneID).bind("plothover", function (event, pos, item) {
+    console.log(event);
+    console.log(pos);
+    console.log(item);
+    alert('test');
+    
+    if (item) {
+        if (previousPoint != item.dataIndex) {
+            previousPoint = item.dataIndex;
+
+            $("#tooltip").remove();
+            var x = item.datapoint[0].toFixed(2),
+                y = item.datapoint[1].toFixed(2);
+
+            showTooltip(item.pageX, item.pageY,
+                        item.series.label + " of " + x + " = " + y);
+        }
+    }
+    else {
+        $("#tooltip").remove();
+        previousPoint = null;            
+    }
+  });
+}
+
+function showTooltip(x, y, contents) {
+  $('<div id="tooltip">' + contents + '</div>').css( {
+      position: 'absolute',
+      display: 'none',
+      top: y + 5,
+      left: x + 5,
+      border: '1px solid #fdd',
+      padding: '2px',
+      'background-color': '#fee',
+      opacity: 0.80
+  }).appendTo("body").fadeIn(200);
 }
 
 /* OpenLayers Functions */
